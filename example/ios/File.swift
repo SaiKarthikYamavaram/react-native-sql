14e1ecce5c8cd155d093e248263e38922c9c847f
SaiKarthikYamavaram <ysaikarthiksep01@gmail.com>
2022-03-16 23:33:26 +0530

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  SqlExample
+//
+
+import Foundation
---
+++ ios/Sql.h
@@ -0,0 +1,11 @@
+#import <React/RCTBridgeModule.h>
+
+#ifdef __cplusplus
+
+#import "react-native-sql.h"
+
+#endif
+
+@interface Sql : NSObject <RCTBridgeModule>
+
+@end
